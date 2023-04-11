438d26b3ad1b8e6ed673d23be72f0c6e8f61d2f3
Lucas Newcomb <pantsbabyy@gmail.com>
2023-04-11 10:51:55 -0400

    Organized

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,23 @@
 #include <cstdlib>
 #include <functional>
-
-// Windows
-#ifdef _WIN32
-#include <Windows.h>
-#include <synchapi.h>
-// Unix, Mac
-#else
 #include <unistd.h>
-#endif // _WIN32
 
-#include "render_buffer.hpp"
-#include "renderer.hpp"
-
-#define WIDTH 100
-#define HEIGHT 25
+#include "gfx/render_buffer.hpp"
+#include "gfx/renderer.hpp"
+#include "common.h"
 
 int main() {
   static Renderer renderer(WIDTH, HEIGHT);
 
   RenderBuffer thing(5, 5, ' ', 0);
-  thing.x = 15;
-  thing.y = 1;
+  thing._x = 15;
+  thing._y = 1;
   thing.drawCircle(3, 3, 2, '#');
   renderer.addRenderBuffer(std::ref(thing));
 
   RenderBuffer thing2(1, 5, ' ', 1);
-  thing2.x = 25;
-  thing2.y = 1;
+  thing2._x = 25;
+  thing2._y = 1;
   thing2.drawLine(1, 1, 1, 5, '@');
   renderer.addRenderBuffer(std::ref(thing2));
 
@@ -37,8 +27,8 @@ int main() {
     renderer.clear();
 
     // Actual Game Events
-    thing.moveLeft(1);
-    thing2.moveLeft(1);
+    thing._moveLeft(1);
+    thing2._moveLeft(1);
 
     // Actual rendering
     renderer.render();
